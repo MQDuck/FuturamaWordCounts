8f2d686f1d54cc488171937015863655d2a230fa
mqduck <mqduck@mqduck.net>
2020-01-28 07:09:38 -0800

    Minor tweaks. Restore transcripts to repository.

--- character_term_frequencies_log.py
+++ character_term_frequencies_log.py
@@ -10,6 +10,9 @@ args = arg_parser.parse_args()
 all_character_sums, word_totals, character_total_sums = \
     tuple(json.load(open('global_term_frequencies_log.json', 'r')))
 character = args.character.lower()
+exponent = 1.4
+exponent_str = ''.join([{'0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸',
+                         '9': '⁹', '+': '⁺', '-': '⁻', '.': '⋅'}[d] for d in str(exponent)])
 
 if character not in all_character_sums:
     print(f'Unknown character: {args.character}')
@@ -18,7 +21,7 @@ if character not in all_character_sums:
 word_sums = all_character_sums[character]
 total_sum = character_total_sums[character]
 
-relative_frequencies = {word: word_sum ** 1.2 / word_totals[word] for word, word_sum in word_sums.items()
+relative_frequencies = {word: word_sum ** exponent / word_totals[word] for word, word_sum in word_sums.items()
                         if word_sum > 1.38 and word_sum / total_sum > 0.0002}
 
 grouped_relative_frequencies = {}
@@ -32,4 +35,4 @@ sorted_relative_frequencies = sorted(grouped_relative_frequencies.items(), key=l
 for i in range(min(20, len(sorted_relative_frequencies))):
     frequency, words = sorted_relative_frequencies[i]
     print(f'#{i + 1:<2} {", ".join(words)} ({frequency:0.5f} : '
-          f'{", ".join([f"{word_sums[word]:>0.4f}/{word_totals[word]:>0.4f}" for word in words])})')
+          f'{", ".join([f"{word_sums[word]:>0.4f}{exponent_str}/{word_totals[word]:>0.4f}" for word in words])})')
--- transcript_processing/search.py
+++ transcript_processing/search.py
@@ -4,13 +4,21 @@ import os
 os.chdir('../transcripts/processed')
 transcripts = [file for file in os.listdir('.') if os.path.isfile(file)]
 
-target_character = "colleen"
+target_character = "agn"
 
-possible_aliases = set()
+exact_matches = 0
+possible_aliases = {}
 for transcript in transcripts:
     character_lines = json.load(open(transcript, 'r', encoding='utf8'))
     for character in character_lines:
-        if target_character in character and target_character != character:
-            possible_aliases.add(f'{transcript[:-5]}: {character}')
+        if target_character in character:
+            if target_character == character:
+                exact_matches += 1
+            elif character in possible_aliases:
+                possible_aliases[character].append(transcript[:-5])
+            else:
+                possible_aliases[character] = [transcript[:-5]]
 
-print('\n'.join(possible_aliases))
+
+print(f'{target_character}: {exact_matches} episodes')
+print('\n'.join([f'{alias} : {", ".join(episodes)}' for alias, episodes in possible_aliases.items()]))
